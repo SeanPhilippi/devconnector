7087ec19e5491c48c8b2d0fad7cf15f87c9aad30
Sean Philippi <sean.philippi@gmail.com>
2019-02-03 21:38:15 -0600

    POST like post route working

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -24,7 +24,7 @@ router.get('/', (req, res) => {
     .sort({ date: -1 })
     .then(posts => res.json(posts))
     .catch(err => 
-      res.status(404).json({nopostfound: 'No posts found'})
+      res.status(404).json({ nopostfound: 'No posts found' })
     );
 });
 
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
   Post.findById(req.params.id)
     .then(post => res.json(post))
     .catch(err => 
-      res.status(404).json({nopostfound: 'No post found with that ID'})
+      res.status(404).json({ nopostfound: 'No post found with that ID' })
     );
 });
 
@@ -67,7 +67,7 @@ router.post(
 // @route   DELETE api/posts/:id
 // @desc    Delete post
 // @access  Private
-router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
   Profile.findOne({user: req.user.id})
     .then(profile => {
       Post.findById(req.params.id)
@@ -75,12 +75,32 @@ router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res)
           // Check for post owner
           if (post.user.toString() !== req.user.id) {
             // 401 authorization error
-            return res.status(401).json({noteauthorized: 'User not authorized'});
+            return res.status(401).json({ noteauthorized: 'User not authorized' });
           }
             // Delete
-            post.remove().then(() => res.json({success: true}));
+            post.remove().then(() => res.json({ success: true }));
         })
-        .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    })
+});
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ alreadyliked: 'User already liked this post' });
+          }
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
     })
 });
 
