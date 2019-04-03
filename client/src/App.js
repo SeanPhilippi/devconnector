1fc9494b47486cb529e72b3e29573eb5f0531003
Sean Philippi <sean.philippi@gmail.com>
2019-04-03 18:10:20 -0500

    token remaining upon reload, now in localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,23 @@ import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './redux/actions/authActions';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+// check for token
+if (localStorage.jwtToken) {
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info and experience
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 
 class App extends Component {
