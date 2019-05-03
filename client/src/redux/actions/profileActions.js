4786e262561eb96c4de3c9ae7d2d43a5b9ae9a73
Sean Philippi <sean.philippi@gmail.com>
2019-05-02 20:38:42 -0500

    updating using async/await

--- client/src/redux/actions/profileActions.js
+++ client/src/redux/actions/profileActions.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, PROFILE_ERROR } from './types';
 
-// Get current profile
-export const getCurrentProfile = () => dispatch => {
-  dispatch(setProfileLoading());
-  axios.get('/api/profile')
-    .then(res => dispatch({
+// Get current user's profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/profile/me');
+
+    dispatch({
       type: GET_PROFILE,
       payload: res.data
-    })
-  )
-  .catch(err => dispatch({
-    type: GET_PROFILE,
-    payload: {}
-  }))
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
 }
 
 // Profile loading
--- client/src/redux/reducers/profileReducer.js
+++ client/src/redux/reducers/profileReducer.js
@@ -11,23 +11,19 @@ const initialState = {
 export default (state = initialState, action) => {
   const { type, payload } = action;
   
-  switch (action.type) {
-    case PROFILE_LOADING:
-      return {
-        ...state,
-        loading: true
-      };
+  switch (type) {
     case GET_PROFILE:
       return {
         ...state,
-        profile: action.payload,
+        profile: payload,
         loading: false
       };
-    case CLEAR_CURRENT_PROFILE:
+    case PROFILE_ERROR:
       return {
         ...state,
-        profile: null
-      }
+        error: payload,
+        loading: false
+      };
     default:
       return state;
   }
